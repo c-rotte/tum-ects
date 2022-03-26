e4b985bab0d0fcd611fe1e53fa4d544ec0532609
rchr1 <54217818+rchr1@users.noreply.github.com>
2022-03-26 15:47:26 +0100

    year -> curriculum_id

--- parser/parser.py
+++ parser/parser.py
@@ -26,16 +26,14 @@ class Parser:
     def __init__(self, database: Database):
         self.database = database
 
-        requests.adapters.DEFAULT_RETRIES = 5
-
-    def switch_to_english(self, PSESSIONID):
+    def _switch_to_english(self, PSESSIONID):
         """switches the web session to english"""
         requests.post("https://campus.tum.de/tumonline/pl/ui/$ctx/wbOAuth2.language",
                       headers={"cookie": f"PSESSIONID={PSESSIONID}"},
                       data={"language": "EN"})
 
-    def parse_node(self, pStpStpNr, node_id, GERMAN_PSESSIONID, ENGLISH_PSESSIONID, parent_keys_de, parent_keys_en,
-                   curriculum_de, curriculum_en):
+    def _parse_node(self, pStpStpNr, node_id, GERMAN_PSESSIONID, ENGLISH_PSESSIONID, parent_keys_de, parent_keys_en,
+                    curriculum_de, curriculum_en):
         """parses a single node in the curriculum tree. For courses: appends it to the list of
         courses, adds course info to curriculum dict. Everything else: adds node in curriculum dict and descents"""
 
@@ -75,13 +73,13 @@ class Parser:
 
                 # create info dicts
                 module_info_de = {
-                    'module_id': module_id,
-                    'ects': 0 if node_ects == '' else float(node_ects),
-                    'weighting_factor': 0 if node_weighting_factor == "" else float(node_weighting_factor),
-                    'title': german_title
+                    "module_id": module_id,
+                    "ects": 0 if node_ects == "" else float(node_ects),
+                    "weighting_factor": 0 if node_weighting_factor == "" else float(node_weighting_factor),
+                    "title": german_title
                 }
                 module_info_en = copy.copy(module_info_de)
-                module_info_en['title'] = english_title
+                module_info_en["title"] = english_title
 
                 add_element_to_dict(curriculum_de, module_info_de, parent_keys_de, module_id)
                 add_element_to_dict(curriculum_en, module_info_en, parent_keys_en, module_id)
@@ -91,20 +89,20 @@ class Parser:
             add_element_to_dict(curriculum_de, {}, parent_keys_de, node_german.span.text.strip())
             add_element_to_dict(curriculum_en, {}, parent_keys_en, node_english.span.text.strip())
             new_node_id = node_german.get("id").replace("kn", "").replace("-toggle", "")
-            self.parse_node(pStpStpNr,
-                            new_node_id,
-                            GERMAN_PSESSIONID,
-                            ENGLISH_PSESSIONID,
-                            parent_keys_de + [node_german.span.text.strip()],
-                            parent_keys_en + [node_english.span.text.strip()],
-                            curriculum_de,
-                            curriculum_en)
+            self._parse_node(pStpStpNr,
+                             new_node_id,
+                             GERMAN_PSESSIONID,
+                             ENGLISH_PSESSIONID,
+                             parent_keys_de + [node_german.span.text.strip()],
+                             parent_keys_en + [node_english.span.text.strip()],
+                             curriculum_de,
+                             curriculum_en)
 
-    def get_for_pStpStpNr(self, pStpStpNr):
+    def _get_for_pStpStpNr(self, pStpStpNr):
         """returns a list of all courses in the given degree and a dict containing the curriculum of the degree"""
-        GERMAN_PSESSIONID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
-        ENGLISH_PSESSIONID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=64))
-        self.switch_to_english(ENGLISH_PSESSIONID)
+        GERMAN_PSESSIONID = "".join(random.choices(string.ascii_uppercase + string.digits, k=64))
+        ENGLISH_PSESSIONID = "".join(random.choices(string.ascii_uppercase + string.digits, k=64))
+        self._switch_to_english(ENGLISH_PSESSIONID)
 
         r_german = requests.get(
             f"https://campus.tum.de/tumonline/pl/ui/$ctx/wbstpcs.showSpoTree?"
@@ -127,7 +125,8 @@ class Parser:
         subtitle_german = soup_german.find("td", {"class": "pageOwner"}).span.text
         subtitle_english = soup_english.find("td", {"class": "pageOwner"}).span.text
 
-        year = title_german[1:5] if title_german[0] == '[' else 'unknown'
+        curriculum_id = \
+            title_german.split("[")[1].split("]")[0] if "[" in title_german and "]" in title_german else "unknown"
 
         print(f"({pStpStpNr}) Parsing {title_german}...")
 
@@ -136,47 +135,49 @@ class Parser:
         degree_info_de = {"degree_id": subtitle_german[0:6],
                           "title": title_german,
                           "subtitle": subtitle_german,
-                          "year": year}
+                          "curriculum_id": curriculum_id}
         degree_info_en = {"degree_id": subtitle_german[0:6],
                           "title": title_english,
                           "subtitle": subtitle_english,
-                          "year": year}
+                          "curriculum_id": curriculum_id}
 
         curriculum_de = {title_german: {}}
         curriculum_en = {title_english: {}}
 
         if main_node:
             main_node_id = main_node.get("id").replace("kn", "").replace("-toggle", "")
-            self.parse_node(pStpStpNr,  # Number of degree
-                            main_node_id,  # start point of recursive descent into curriculum
-                            GERMAN_PSESSIONID,  # session ID for german requests
-                            ENGLISH_PSESSIONID,  # session ID for english requests
-                            [title_german],  # acc for the keys in recursive descent, used for german curriculum
-                            [title_english],  # acc for the keys in recursive descent, used for english curriculum
-                            curriculum_de,  # dict used to store the german curriculum
-                            curriculum_en)  # dict used to store the english curriculum
-        degree_info_de['curriculum'] = curriculum_de
-        degree_info_en['curriculum'] = curriculum_en
+            self._parse_node(pStpStpNr,  # Number of degree
+                             main_node_id,  # start point of recursive descent into curriculum
+                             GERMAN_PSESSIONID,  # session ID for german requests
+                             ENGLISH_PSESSIONID,  # session ID for english requests
+                             [title_german],  # acc for the keys in recursive descent, used for german curriculum
+                             [title_english],  # acc for the keys in recursive descent, used for english curriculum
+                             curriculum_de,  # dict used to store the german curriculum
+                             curriculum_en)  # dict used to store the english curriculum
+        degree_info_de["curriculum"] = curriculum_de
+        degree_info_en["curriculum"] = curriculum_en
         return degree_info_de, degree_info_en
+
+    def _process_pStpStpNr(self, pStpStpNr):
+        print(f"Checking pStpStpNr {pStpStpNr}...")
+        degree_info_de, degree_info_en = self._get_for_pStpStpNr(pStpStpNr)
+        # remove old data
+        self.database.remove_degree(pStpStpNr)
+        self.database.remove_curriculum(pStpStpNr, language="german")
+        self.database.remove_curriculum(pStpStpNr, language="english")
+        if not degree_info_de or not degree_info_en:
+            return
+        # add new data
+        degree_info_de["pStpStpNr"] = pStpStpNr
+        degree_info_en["pStpStpNr"] = pStpStpNr
+        self.database.add_curriculum(degree_info_de, language="german")
+        self.database.add_curriculum(degree_info_en, language="english")
 
     def run(self):
         # check all numbers
         for pStpStpNr in range(0, 10000):
-            print(f"Checking pStpStpNr {pStpStpNr}...")
             try:
-                degree_info_de, degree_info_en = self.get_for_pStpStpNr(pStpStpNr)
-
-                # remove old data
-                self.database.remove_degree(pStpStpNr)
-                self.database.remove_curriculum(pStpStpNr, language='german')
-                self.database.remove_curriculum(pStpStpNr, language='english')
-                if not degree_info_de or not degree_info_en:
-                    continue
-                # add new data
-                degree_info_de["pStpStpNr"] = pStpStpNr
-                degree_info_en["pStpStpNr"] = pStpStpNr
-                self.database.add_curriculum(degree_info_de, language='german')
-                self.database.add_curriculum(degree_info_en, language='english')
+                self._process_pStpStpNr(pStpStpNr)
             except Exception as e:
                 return e
         return None
