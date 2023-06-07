270d07065e6531c46e28d86183934c688a7dcfc5
Maximilian Kienitz <max@kienitz.dev>
2023-06-07 20:13:52 +0200

    Cleanup GQL

--- tumgrades/src/routes/+page.server.ts
+++ tumgrades/src/routes/+page.server.ts
@@ -1,13 +1,5 @@
-import { loadSchemaSync } from '@graphql-tools/load';
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { printSchema } from 'graphql';
-
 export async function load() {
-	const typeDefs = loadSchemaSync('./../schema/**/*.graphql', {
-		loaders: [new GraphQLFileLoader()]
-	});
-
 	return {
-		gqlTypeDefs: printSchema(typeDefs)
+		helloWorld: 'Hello, world!'
 	};
 }
--- tumgrades/src/routes/+page.svelte
+++ tumgrades/src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	export let data: PageData;
 </script>
 
 <h1>Welcome to TUM-ECTS</h1>
-<p>Here are our GraphQL schemas:</p>
-<pre>{data.gqlTypeDefs}</pre>
+<p>This is work in progress :(</p>
