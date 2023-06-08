c4a8b8a1953ad91fe63ec510a37ad571e1595cfb
Maximilian Kienitz <max@kienitz.dev>
2023-06-08 19:18:53 +0200

    Add sample prisma access

--- tumgrades/src/routes/+page.server.ts
+++ tumgrades/src/routes/+page.server.ts
@@ -1,5 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
 export async function load() {
+	const prisma = new PrismaClient();
+	const modules = await prisma.module.findMany();
 	return {
-		helloWorld: 'Hello, world!'
+		modules: modules
 	};
 }
--- tumgrades/src/routes/+page.svelte
+++ tumgrades/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+	export let data;
 </script>
 
 <h1>Welcome to TUM-ECTS</h1>
 <p>This is work in progress :(</p>
+<ul>
+	{#each data.modules as module}
+		<li>{module.titleDE}</li>
+	{/each}
+</ul>
